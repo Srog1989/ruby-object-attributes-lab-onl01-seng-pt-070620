49e3375b7c676f112bcc7af6848edde43845ec4e
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-22 02:12:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 class Dog
 
- def initialize(dog_name)
+ def name(dog_name)
   @name = dog_name
 end
   
   def name(dog_name)
     @name
+  
+  def (dog_breed)
+    @breed = dog_breed
+    
+    
   end
 end
